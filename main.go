6ffcb52a5d010728fba5cf879416753135967a72
Christopher Ndung'u <dev.kiratu@gmail.com>
2025-08-06 12:27:49 +0300

    add help and exit commands for the repl

--- main.go
+++ main.go
@@ -7,8 +7,46 @@ import (
 	"strings"
 )
 
+
+var supportedCommands map[string]cliCommand = map[string]cliCommand{
+	"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+	},
+	"help": {
+		name: "help",
+		description: "Displays a help message",
+		callback: commandHelp,
+	},
+}
+
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	help := `
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+	`
+	fmt.Println(help)
+	return  nil
 }
 
 func main() {
@@ -16,8 +54,14 @@ func main() {
 		fmt.Print("Pokedex > ")
 	for scanner.Scan() {
 		text := scanner.Text()
-		uInput := cleanInput(text)
-		fmt.Println("Your command was:", uInput[0])
+		userInput := cleanInput(text)
+
+		if c, ok := supportedCommands[userInput[0]]; ok {
+			c.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+
 		fmt.Print("Pokedex > ")
 	}
 }
