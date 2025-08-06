56694cbc15f123156e82a52f6cd9beccea696ce4
Christopher Ndung'u <dev.kiratu@gmail.com>
2025-08-06 16:55:24 +0300

    add interactivity to the repl by calling the pokemon api

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -20,26 +22,105 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback: commandHelp,
 		},
+		"map": {
+			name: "map",
+			description: "Displays the names of (next) 20 location areas in the Pokemon world",
+			callback: commandMap,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Displays the names of previous 20 location areas in the Pokemon world",
+			callback: commandMapb,
+		},
 	}
 }
 
 type cliCommand struct {
 	name string
 	description string
-	callback func() error
+	callback func(*pagesNave) error
+}
+
+type pokeResult struct {
+	Count int `json:"count"`
+	Next string `json:"next"`
+	Previous string `json:"previous"`
+	Results []struct{
+		Name string `json:"name"`
+		Url string `json:"url"`
+		} `json:"results"`
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
-func commandExit() error {
+type pagesNave struct {
+	next string
+	previous string
+}
+
+var nav = &pagesNave{}
+
+func commandMap(nav *pagesNave) error {
+	url := "https://pokeapi.co/api/v2/location-area"
+	if nav.next != "" {
+		url = nav.next
+	}
+	err := makeApiRequest(url)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func makeApiRequest(url string) error {
+// prepare request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	var apiResults pokeResult
+	decoder:= json.NewDecoder(res.Body)
+	err = decoder.Decode(&apiResults)
+	if err != nil {
+		return err
+	}
+	// fmt.Println(apiResults)
+	nav.next = apiResults.Next
+	nav.previous = apiResults.Previous
+	for _, item := range apiResults.Results {
+		fmt.Println(item.Name)
+	}
+	return nil
+}
+
+func commandMapb(nav *pagesNave) error {
+	if nav.previous == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	err := makeApiRequest(nav.previous)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func commandExit(nav *pagesNave) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(nav *pagesNave) error {
 	help := `
 Welcome to the Pokedex!
 Usage:
@@ -62,9 +143,9 @@ func main() {
 			fmt.Print("Pokedex > ")
 			continue
 		}
-		
+
 		if c, ok := getCommands()[userInput[0]]; ok {
-			c.callback()
+			c.callback(nav)
 		} else {
 			fmt.Println("Unknown command")
 		}
